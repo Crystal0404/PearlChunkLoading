85228eae294a1962897a876732c5653308813a07
Crystal0404 <syh040325@163.com>
2025-04-21 22:31:36 +0800

    init

---
+++ src/main/java/crystal0404/pearlchunkloading/mixin/EnderPearlEntityMixin.java
@@ -0,0 +1,9 @@
+package crystal0404.pearlchunkloading.mixin;
+
+import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(EnderPearlEntity.class)
+public abstract class EnderPearlEntityMixin {
+
+}
